9fce09d218a10ad6cde621ab9d8714b4cfc01427
Paul Semel <paul.semel@lse.epita.fr>
2017-07-21 11:02:59 +0200

    profiling: added profiling class and methods
    Signed-off-by: Paul Semel <paul.semel@lse.epita.fr>

---
+++ profiling.py
@@ -0,0 +1,27 @@
+import cProfile
+import sys
+import pstats
+import tspanner
+
+# for the set_method, just put the name of the method you want to call
+# like 'compute_wspd'
+
+class Profiling:
+    def __init__(self):
+        self.prof = cProfile.Profile()
+    def set_args(self, coef, filename):
+        self.coef = coef
+        self.filename = filename
+    def set_method(self, name):
+        self.method = name
+    def run(self):
+        method = getattr(tspanner, self.method)
+        self.prof.enable()
+        #ret = self.prof.runcall(method, self.coef, self.filename)
+        ret = method(self.coef, self.filename)
+        self.prof.disable()
+        return ret
+    def print_stats(self):
+        ps = pstats.Stats(self.prof, stream=sys.stdout)
+        ps.print_stats()
+
